29c98af0c56547db820a16bafe154fd4ea65e2ef
Brian McConney <briancamcc@gmail.com>
2021-09-02 10:25:52 -0400

    "Finally got Spring boot to connect to MSSQL"

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,21 @@
 package com.example.demo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
 
+// public class DemoApplication implements CommandLineRunner{
 public class DemoApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		
@@ -13,7 +23,20 @@ public class DemoApplication {
 		 * in pom.xml, comment out the spring-boot-starter-data-jpa dependency
 		 * until you are ready to connect to a DB
 		 */
-		
 	}
+	
+	/*
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
+
+	
+	@Override
+    public void run(String... args) throws Exception {
+        String sql = "SELECT * FROM students";
+        List<Student> students = jdbcTemplate.query(sql,
+                BeanPropertyRowMapper.newInstance(Student.class));
+         
+        students.forEach(System.out :: println);
+    }*/
 		
 }
